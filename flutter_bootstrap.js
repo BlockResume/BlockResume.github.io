3898890c8e0124f9c1c775f831b77747e05852cf
Claes Heise <sadclat@gmail.com>
2025-06-26 14:22:47 +0200

    Now with secure storage

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "407428817"
+    serviceWorkerVersion: "661416713"
   }
 });
