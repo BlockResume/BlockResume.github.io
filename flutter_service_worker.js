6127b137b9da908191de6f09a7ecef7e909b3fe8
Claes Heise <sadclat@gmail.com>
2025-06-26 13:36:59 +0200

    Rebuild project

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ebc9d157b03b1681aad17fb80bee9f13
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "6b9211394ecbbc76c4cdebd35c635141",
+"flutter_bootstrap.js": "9281d6ef0785b56a9f7c703bfdef50f9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
